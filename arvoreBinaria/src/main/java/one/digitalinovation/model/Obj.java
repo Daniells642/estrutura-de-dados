d095ce6e79a90227ded738b89aab8704537ab1c5
Daniel <daniells642@hotmail.com>
2023-04-01 20:09:47 -0300

    update

--- arvoreBinaria/src/main/java/one/digitalinovation/model/Obj.java
+++ arvoreBinaria/src/main/java/one/digitalinovation/model/Obj.java
@@ -4,7 +4,8 @@ import java.util.Objects;
 
 public class Obj extends ObjArvore<Obj>{
     Integer meuValor;
-    public Obj(Integer meuValor) {
+
+    public Obj(Integer meuValor){
         this.meuValor = meuValor;
     }
 
@@ -22,27 +23,30 @@ public class Obj extends ObjArvore<Obj>{
     }
 
     @Override
+    public int CompareTo(Obj outro) {
+        return 0;
+    }
+
+    @Override
     public int hashCode() {
         return Objects.hash(meuValor);
     }
+
     @Override
-    public int CompareTo(Obj outro) {
+    public int compareTo(Obj outro) {
         int i = 0;
 
-        if (this.meuValor > outro.meuValor){
+        if(this.meuValor > outro.meuValor){
             i = 1;
-        }else if (this.meuValor < outro.meuValor){
+        }else if(this.meuValor < outro.meuValor){
             i = -1;
         }
+
         return i;
     }
+
     @Override
     public String toString() {
         return meuValor.toString();
-    }
-
-    @Override
-    public int compareTo(Obj o) {
-        return 0;
     }
 }
